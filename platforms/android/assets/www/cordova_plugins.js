2afacfae09377e81d93bdcb382ea5f6e38de3e60
Dora3monZ <haribinhamdan90@gmail.com>
2016-04-01 11:20:34 +0800

    t

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.sms"
         ]
+    },
+    {
+        "file": "plugins/com.jcjee.plugins.emailcomposer/www/EmailComposer.js",
+        "id": "com.jcjee.plugins.emailcomposer.EmailComposer",
+        "clobbers": [
+            "EmailComposer"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.cordova.plugins.sms": "0.1.6"
+    "com.cordova.plugins.sms": "0.1.6",
+    "com.jcjee.plugins.emailcomposer": "1.4.6"
 };
 // BOTTOM OF METADATA
 });
