0d66a6609c3fbee50a7dc7b3da5fcfa563fff299
Shahiran <mshahiranzabedin@gmail.com>
2016-04-05 11:26:49 +0800

    commit 5

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,28 +6,12 @@ module.exports = [
         "clobbers": [
             "window.sms"
         ]
-    },
-    {
-        "file": "plugins/com.jcjee.plugins.emailcomposer/www/EmailComposer.js",
-        "id": "com.jcjee.plugins.emailcomposer.EmailComposer",
-        "clobbers": [
-            "EmailComposer"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-        "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.cordova.plugins.sms": "0.1.6",
-    "com.jcjee.plugins.emailcomposer": "1.4.6",
-    "cordova-plugin-x-socialsharing": "5.0.12-dev"
+    "com.cordova.plugins.sms": "0.1.6"
 };
 // BOTTOM OF METADATA
 });
