67d7f709912b9e6d66e9b42da0c53a4534e01ea6
Shahiran <mshahiranzabedin@gmail.com>
2016-04-05 15:31:26 +0800

    system suh commit

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -3,7 +3,6 @@ module.exports = [
     {
         "file": "plugins/com.cordova.plugins.sms/www/sms.js",
         "id": "com.cordova.plugins.sms.Sms",
-        "pluginId": "com.cordova.plugins.sms",
         "clobbers": [
             "window.sms"
         ]
@@ -11,7 +10,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
-        "pluginId": "cordova-plugin-splashscreen",
         "clobbers": [
             "navigator.splashscreen"
         ]
@@ -19,7 +17,6 @@ module.exports = [
     {
         "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
         "id": "ionic-plugin-keyboard.keyboard",
-        "pluginId": "ionic-plugin-keyboard",
         "clobbers": [
             "cordova.plugins.Keyboard"
         ],
@@ -28,7 +25,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/contacts.js",
         "id": "cordova-plugin-contacts.contacts",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "navigator.contacts"
         ]
@@ -36,7 +32,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/Contact.js",
         "id": "cordova-plugin-contacts.Contact",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "Contact"
         ]
@@ -44,7 +39,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
         "id": "cordova-plugin-contacts.ContactAddress",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactAddress"
         ]
@@ -52,7 +46,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
         "id": "cordova-plugin-contacts.ContactError",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactError"
         ]
@@ -60,7 +53,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
         "id": "cordova-plugin-contacts.ContactField",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactField"
         ]
@@ -68,7 +60,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
         "id": "cordova-plugin-contacts.ContactFindOptions",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactFindOptions"
         ]
@@ -76,7 +67,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
         "id": "cordova-plugin-contacts.ContactName",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactName"
         ]
@@ -84,7 +74,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
         "id": "cordova-plugin-contacts.ContactOrganization",
-        "pluginId": "cordova-plugin-contacts",
         "clobbers": [
             "ContactOrganization"
         ]
@@ -92,7 +81,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
         "id": "cordova-plugin-contacts.ContactFieldType",
-        "pluginId": "cordova-plugin-contacts",
         "merges": [
             ""
         ]
@@ -100,7 +88,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "Camera"
         ]
@@ -108,7 +95,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
         "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "CameraPopoverOptions"
         ]
@@ -116,7 +102,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/Camera.js",
         "id": "cordova-plugin-camera.camera",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "navigator.camera"
         ]
@@ -124,7 +109,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
         "id": "cordova-plugin-camera.CameraPopoverHandle",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "CameraPopoverHandle"
         ]
@@ -132,7 +116,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-vibration/www/vibration.js",
         "id": "cordova-plugin-vibration.notification",
-        "pluginId": "cordova-plugin-vibration",
         "merges": [
             "navigator.notification",
             "navigator"
@@ -141,7 +124,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
         "clobbers": [
             "device"
         ]
@@ -149,13 +131,11 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     },
     {
         "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
         "id": "cordova-plugin-inappbrowser.inappbrowser",
-        "pluginId": "cordova-plugin-inappbrowser",
         "clobbers": [
             "cordova.InAppBrowser.open",
             "window.open"
@@ -164,7 +144,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
         "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "pluginId": "cordova-plugin-x-socialsharing",
         "clobbers": [
             "window.plugins.socialsharing"
         ]
@@ -174,7 +153,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.cordova.plugins.sms": "0.1.6",
-    "cordova-plugin-splashscreen": "3.2.2-dev",
+    "cordova-plugin-splashscreen": "3.1.0",
     "ionic-plugin-keyboard": "1.0.8",
     "cordova-plugin-googleplayservices": "19.0.3",
     "cordova-plugin-contacts": "2.0.1",
@@ -185,7 +164,7 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.1",
     "cordova-plugin-transport-security": "0.1.1",
     "cordova-plugin-inappbrowser": "1.3.0",
-    "cordova-plugin-x-socialsharing": "5.0.10"
-}
+    "cordova-plugin-x-socialsharing": "5.0.12-dev"
+};
 // BOTTOM OF METADATA
 });
